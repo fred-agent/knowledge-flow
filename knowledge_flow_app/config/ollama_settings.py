3b693eda20c738772646db28d537f16e661b17f8
Kévin DENIS <kevin.denis@thalesgroup.com>
2025-05-28 18:23:45 +0200

    #11 Add image processing in pdf to markdown processor

--- knowledge_flow_app/config/ollama_settings.py
+++ knowledge_flow_app/config/ollama_settings.py
@@ -16,12 +16,13 @@ import logging
 from typing import Optional
 from pydantic_settings import BaseSettings
 from pydantic import Field
-import os
+
 logger = logging.getLogger(__name__)
 
-class EmbeddingOllamaSettings(BaseSettings):
-    embedding_model_name: str = Field(..., validation_alias="OLLAMA_EMBEDDING_MODEL_NAME")
+class OllamaSettings(BaseSettings):
     api_url: Optional[str] = Field(default=None, validation_alias="OLLAMA_API_URL")
+    embedding_model_name: str = Field(..., validation_alias="OLLAMA_EMBEDDING_MODEL_NAME")
+    vision_model_name: Optional[str] = Field(default=None, validation_alias="OLLAMA_VISION_MODEL_NAME")
 
     model_config = {
         "extra": "ignore" # allow extra environment variable 
